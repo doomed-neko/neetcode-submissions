15680af11c38bbcb30eaba66fc1114f413e2a584
haie <79870712+doomed-neko@users.noreply.github.com>
2026-04-25 22:46:52 +0300

    Add: pow-x-n - submission-4

---
+++ Data Structures & Algorithms/pow-x-n/submission-4.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn my_pow(x: f64, n: i32) -> f64 {
+        let mut p = 1.;
+        if x == 0.{return 0.}
+        if n==0 {return 1.}
+        for i in 0..n.abs(){
+            p *= x;
+        }
+        if n>=0{p} else{1./p}
+    }
+}
