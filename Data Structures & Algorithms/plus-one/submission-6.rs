e34a486fd4045577e0a4c93bbd686f9323248044
haie <79870712+doomed-neko@users.noreply.github.com>
2026-04-25 21:46:19 +0300

    Add: plus-one - submission-6

---
+++ Data Structures & Algorithms/plus-one/submission-6.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+        for i in digits.iter_mut().rev() {
+            if i < &mut 9 {
+                *i += 1;
+                return digits;
+            }
+            *i = 0;
+        }
+        [vec![1], digits].into_iter().flatten().collect()
+    }
+}
