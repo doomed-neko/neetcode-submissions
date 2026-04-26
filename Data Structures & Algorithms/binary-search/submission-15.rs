c731dde1d23dad55a1c7abf7294d6147bd69e457
haie <79870712+doomed-neko@users.noreply.github.com>
2026-04-26 09:37:56 +0300

    Add: binary-search - submission-15

---
+++ Data Structures & Algorithms/binary-search/submission-15.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        fn my_search(l: i32, r: i32, nums: &[i32], target: i32) -> i32 {
+            if l > r {
+                return -1;
+            }
+            let mid = l + (r - l) / 2;
+            match nums[mid as usize].cmp(&target) {
+                std::cmp::Ordering::Less => my_search(mid + 1, r, nums, target),
+                std::cmp::Ordering::Equal => mid,
+                std::cmp::Ordering::Greater => my_search(l, mid - 1, nums, target),
+            }
+        }
+        my_search(0, nums.len() as i32 - 1, &nums, target)
+    }
+}
