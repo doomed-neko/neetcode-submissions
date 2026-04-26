6477737e7d25617062a1a5a09f372aa7e4d7cd2d
haie <79870712+doomed-neko@users.noreply.github.com>
2026-04-26 11:19:53 +0300

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut l = 0usize;
+        let mut r = numbers.len() - 1;
+        while l <= r {
+            match (numbers[l] + numbers[r]).cmp(&target) {
+                std::cmp::Ordering::Greater => r -= 1,
+                std::cmp::Ordering::Equal => return vec![l as i32 + 1, r as i32 + 1],
+                std::cmp::Ordering::Less => l += 1,
+            }
+        }
+        vec![]
+    }
+}
