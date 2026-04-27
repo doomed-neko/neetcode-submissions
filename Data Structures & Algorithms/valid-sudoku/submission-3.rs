3799061d48711c9f02f537093174540e33f28e04
haie <79870712+doomed-neko@users.noreply.github.com>
2026-04-27 12:14:45 +0300

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        let mut rows = [0u16; 9];
+        let mut cols = [0u16; 9];
+        let mut sqrs = [0u16; 9];
+        for (row_index, row) in board.iter().enumerate() {
+            for (col_index, &col) in row.iter().enumerate() {
+                if col == '.' {
+                    continue;
+                }
+                let col = col as u16;
+                let bit = 1 << col;
+                let sqr_index = (row_index / 3) * 3 + col_index / 3;
+                if rows[row_index] & bit != 0
+                    || cols[col_index] & bit != 0
+                    || sqrs[sqr_index] & bit != 0
+                {
+                    return false;
+                }
+                rows[row_index] |= bit;
+                cols[col_index] |= bit;
+                sqrs[sqr_index] |= bit;
+            }
+        }
+        true
+    }
+}
